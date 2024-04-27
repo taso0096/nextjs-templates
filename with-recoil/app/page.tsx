baeee9003f81d262599818e948b175b11791b1dc
taso0096 <jin2931121@yahoo.co.jp>
2024-04-27 10:58:08 +0900

    WIP

--- with-recoil/app/page.tsx
+++ with-recoil/app/page.tsx
@@ -1,5 +1,5 @@
 import * as styles from "./page.css";
 
 export default function Home() {
-  return <main className={styles.main}>nextjs-template</main>;
+  return <main className={styles.main}>nextjs-template with-recoil</main>;
 }
